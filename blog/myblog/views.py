ee8df9b73c964ff344ccd6ebd143e573d67e5fe6
slayzerg01 <bagdat250716@gmail.com>
2023-03-25 20:57:28 +0500

    first commit my django project

---
+++ blog/myblog/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.views import View
+from django.core.paginator import Paginator
+from .models import Post
+
+
+class MainView(View):
+    def get(self, request, *args, **kwargs):
+        posts = Post.objects.all()
+        paginator = Paginator(posts, 6)
+
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+
+        return render(request, 'myblog/home.html', context={
+            'page_obj': page_obj
+        })
+
